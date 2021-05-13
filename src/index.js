33db42377f5a90c4f80f4263d83ddce2c3818720
conestoga-web <mar.26.2019@gmail.com>
2021-05-13 20:00:44 -0300

    git deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import rootReducer from './modules';
 import ReduxThunk from 'redux-thunk';
 import './index.css';
