78e5cef267552d16331164ee1dcfe851ddf47648
conestoga-web <mar.26.2019@gmail.com>
2021-05-14 06:50:12 -0300

    git deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Suspense} from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 //const AppBar = React.lazy(() => import('./components/AppBar'));
-//const Home = React.lazy(() => import('./pages/Home'));
+const Home = React.lazy(() => import('./pages/Home'));
 const People = React.lazy(() => import('./pages/People'));
 const PeopleDetail = React.lazy(() => import('./pages/PeopleDetail'));
 const Planets = React.lazy(() => import('./pages/Planets'));
@@ -13,13 +13,13 @@ const MovieDetail = React.lazy(() => import('./pages/MovieDetail'));
 function App() {
   return (
     <Suspense fallback={<div>Welcome, loading...</div>}>
-      <Router basename={'/starwars_films_info'}>
-        {/* <Route component={Home} path="/" exact={true} /> */}
+      <Router basename='/starwars_films_info'>
+        <Route component={Home} path="/" exact={true} />
         <Route component={People} path="/people" />
         <Route component={PeopleDetail} path="/person" />  
         <Route component={Planets} path="/planets" />
         <Route component={PlanetDetail} path="/planet" />
-        <Route component={Movies} path="/" exact={true} />
+        <Route component={Movies} path="/movies" exact={true} />
         <Route component={MovieDetail} path="/movie" />
       </Router>
     </Suspense>
